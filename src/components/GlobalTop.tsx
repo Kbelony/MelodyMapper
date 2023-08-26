604ffdf9edce3faf1b7182bd795bfe74afd20b53
Kalvin <kalvin.sgb@outlook.com>
2023-08-26 13:11:22 +0200

    🚧 : add background-image top artist

--- src/components/GlobalTop.tsx
+++ src/components/GlobalTop.tsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const GlobalTop = () => {
-  // Extraire l'access token de l'URL
   const urlParams = new URLSearchParams(window.location.hash.substring(1));
   const access_token = urlParams.get("access_token");
 
-  // Enregistrer l'access token dans le local storage
   localStorage.setItem("access_token", access_token);
+
+  const [topArtist, setTopArtist] = useState(null);
+
   useEffect(() => {
     const accessToken = localStorage.getItem("access_token") || "";
     const apiUrl = "https://api.spotify.com/v1/me/top/artists";
@@ -20,11 +21,12 @@ const GlobalTop = () => {
           },
           params: {
             time_range: "medium_term",
-            limit: 10,
+            limit: 15,
           },
         });
 
         console.log("Top Artists:", response.data.items);
+        setTopArtist(response.data.items[0]); // Set the first artist from the response
       } catch (error) {
         console.error("Error fetching top artists:", error);
       }
@@ -35,7 +37,21 @@ const GlobalTop = () => {
     }
   }, []);
 
-  return <div>GlobalTop</div>;
+  return (
+    <div className="global-component">
+      <div className="desktop-part">
+        <p>desktop</p>
+      </div>
+      <div
+        className="mobile-part"
+        style={{
+          backgroundImage: topArtist ? `url(${topArtist.images[0].url})` : "",
+        }}
+      >
+        <p>mobile</p>
+      </div>
+    </div>
+  );
 };
 
 export default GlobalTop;
