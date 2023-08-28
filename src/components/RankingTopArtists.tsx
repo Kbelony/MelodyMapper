075298840309ba05039a16a2f34afe3cba3d0e87
Kalvin <kalvin.sgb@outlook.com>
2023-08-29 01:18:22 +0200

    🚧 : reorganise components Ranking

--- src/components/RankingTopArtists.tsx
+++ src/components/RankingTopArtists.tsx
@@ -1,75 +1,38 @@
 import { useContext, useState } from "react";
 import { LanguageContext } from "./LanguageContext";
 import back from "../assets/images/Vector.svg";
-import down from "../assets/images/Vector-down.svg";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import ArtistsTop from "./ArtistsTop";
+import TracksTop from "./TracksTop";
 
 const RankingTopArtists = () => {
   const { language } = useContext(LanguageContext) || { language: "en" };
   const history = useNavigate();
-  const accessToken = localStorage.getItem("access_token") || "";
-
-  interface ArtistType {
-    name: string;
-    images: { url: string }[];
-    genres: string[];
-    popularity: number;
-    external_urls: { spotify: string };
-    id: string;
-  }
 
   interface Translations {
     [key: string]: {
-      slogan: string;
-      topArtist: string;
       navigate: string;
-      discover: string;
+      slogan: string;
     };
   }
 
   const translations: Translations = {
     fr: {
-      slogan: "Place au <span>stastisques</span> !",
-      topArtist: "Classement de vos artistes du moment",
       navigate: "Page précédente",
-      discover: "Découvrir des artistes similaires ?",
+      slogan: "Place au <span>stastisques</span> !",
     },
     en: {
       slogan: "Make way for <span>stastics</span> !",
-      topArtist: "Ranking your artists of the moment",
       navigate: "Previous page",
-      discover: "Discover similar artists ?",
     },
   };
 
   const translationKey = language || "en";
-  const { slogan, topArtist, navigate, discover } =
-    translations[translationKey];
+  const [enabled, setEnabled] = useState(false);
+  const { navigate, slogan } = translations[translationKey];
 
-  const topArtists = JSON.parse(localStorage.getItem("top_artist") || "[]");
-
-  const [expandedArtistId, setExpandedArtistId] = useState<string | null>(null);
-  const [relatedArtists, setRelatedArtists] = useState<ArtistType[]>([]);
-
-  const handleArtistClick = async (artist: ArtistType) => {
-    const relatedArtistsUrl = `https://api.spotify.com/v1/artists/${artist.id}/related-artists`;
-
-    try {
-      const response = await axios.get(relatedArtistsUrl, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      const relatedArtists = response.data.artists.slice(0, 3);
-      setRelatedArtists(relatedArtists);
-
-      setExpandedArtistId(artist.id);
-    } catch (error) {
-      console.error("Error fetching related artists:", error);
-    }
-  };
+  const showArtistsTop = !enabled;
+  const showTracksTop = enabled;
 
   return (
     <div className="ranking-artists-component">
@@ -86,64 +49,27 @@ const RankingTopArtists = () => {
             <img className="mr-2 mt-1" src={back} alt="" />
             <p className="text-sm">{navigate}</p>
           </div>
+          <div className="toggle-switch">
+            <label className="inline-flex relative items-center mr-5 cursor-pointer">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={enabled}
+                readOnly
+              />
+              <div
+                onClick={() => {
+                  setEnabled(!enabled);
+                }}
+                className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+              ></div>
+              <span className="ml-2 text-sm font-medium text-gray-900">ON</span>
+            </label>
+          </div>
           <div className="flex justify-center items-center h-screen mb-8">
             <div className="container content-border">
-              <div className="top-artist">
-                <h3 className="text-center text-lg mb-8 mt-4">{topArtist}</h3>
-                <div className="ranking flex flex-col">
-                  {topArtists.map((artist: ArtistType, index: number) => (
-                    <div className="items flex flex-col mb-4" key={artist.id}>
-                      <div className="top-info flex items-center">
-                        <h1 className="ml-4 mr-5 mt-4 text-2xl">
-                          {index + 1}.
-                        </h1>
-                        <img src={artist.images[2].url} alt="" />
-                        <h1 className="mt-4 ml-4">{artist.name}</h1>{" "}
-                        <img
-                          className="flex-end ml-auto arrow mr-4"
-                          src={down}
-                          alt=""
-                          onClick={() => {
-                            if (expandedArtistId === artist.id) {
-                              setExpandedArtistId(null);
-                            } else {
-                              setExpandedArtistId(artist.id);
-                              handleArtistClick(artist);
-                            }
-                          }}
-                        />
-                      </div>
-                      {expandedArtistId === artist.id && (
-                        <div className="related-artists-container">
-                          <h6 className="text-center text-xs mt-5">
-                            {discover}
-                          </h6>
-                          <div className="related-artists md:justify-center ml-3.5 flex">
-                            {relatedArtists.map((relatedArtist: ArtistType) => (
-                              <Link
-                                key={relatedArtist.id}
-                                to={relatedArtist.external_urls.spotify}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="related-artist flex flex-col items-center text-center m-1 p-4 pb-0"
-                              >
-                                <img
-                                  src={relatedArtist.images[0].url}
-                                  alt={relatedArtist.name}
-                                  className="mb-2"
-                                />
-                                <h6 className="text-center text-xs mt-2">
-                                  {relatedArtist.name}
-                                </h6>
-                              </Link>
-                            ))}
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
+              {showTracksTop && <TracksTop></TracksTop>}
+              {showArtistsTop && <ArtistsTop></ArtistsTop>}
             </div>
           </div>
         </div>
---
+++ src/components/TracksTop.tsx
@@ -0,0 +1,88 @@
+import { useContext, useEffect, useState } from "react";
+import { LanguageContext } from "./LanguageContext";
+
+import axios from "axios";
+
+const TracksTop = () => {
+  const { language } = useContext(LanguageContext) || { language: "en" };
+  const accessToken = localStorage.getItem("access_token") || "";
+
+  interface Track {
+    id: string;
+    album: {
+      images: {
+        url: string;
+      }[];
+    };
+    name: string;
+  }
+
+  interface Translations {
+    [key: string]: {
+      topArtist: string;
+    };
+  }
+
+  const translations: Translations = {
+    fr: {
+      topArtist: "Classement de vos titres du moment",
+    },
+    en: {
+      topArtist: "Ranking your tracks of the moment",
+    },
+  };
+
+  const translationKey = language || "en";
+  const { topArtist } = translations[translationKey];
+  const [topTracks, setTopTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.spotify.com/v1/me/top/tracks",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+            params: {
+              time_range: "medium_term",
+              limit: 15,
+            },
+          }
+        );
+
+        setTopTracks(response.data.items);
+        console.log(response.data.items);
+      } catch (error) {
+        console.error("Error fetching top tracks:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="top-tracks">
+      <h3 className="text-center text-lg mb-8 mt-4">{topArtist}</h3>
+      <div className="ranking flex flex-col">
+        {topTracks.map((track: Track, index: number) => (
+          <div className="items flex flex-col mb-6" key={track.id}>
+            <div className="top-info flex items-center">
+              <h1 className="ml-4 mr-5 mt-4 text-2xl">
+                {(index + 1).toLocaleString("en-US", {
+                  minimumIntegerDigits: 2,
+                })}
+                .
+              </h1>
+              <img src={track.album.images[2].url} alt="" />
+              <h1 className="mt-4 ml-4">{track.name}</h1>{" "}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TracksTop;
